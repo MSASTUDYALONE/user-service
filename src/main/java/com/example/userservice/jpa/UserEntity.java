47cfaf8274411e8a29b69c44b1b220ad99f8333a
sesam-me <lsi6601@gmail.com>
2024-02-16 23:56:56 +0900

    Feat: mariaDB로 동기화

--- src/main/java/com/example/userservice/jpa/UserEntity.java
+++ src/main/java/com/example/userservice/jpa/UserEntity.java
@@ -14,7 +14,7 @@ public class UserEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, length = 50, unique = true)
+//    @Column(nullable = false, length = 50, unique = true)
     private String email;
 
     @Column(nullable = false, length = 50)
